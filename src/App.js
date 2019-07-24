6303caf1991ad5142b2e0462ecb6e040b71665e1
Marlon Avery <team@rekure.com>
2019-07-24 13:40:17 -0400

    moved reducers to allReducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './actions/'
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
   return (
     <div className="App">
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      {isLogged ? <h2>If logged in: Vanish</h2> : ''}
       
     </div>
   );
